f6e292fe8de9cf101ef54db241444b0d5b5f5323
Alex <alexlynd@protonmail.com>
2020-10-19 11:32:11 -0700

    asf

--- game/game.ino
+++ game/game.ino
@@ -12,6 +12,11 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 int cells[128][64];
 int i; int j;
 
+// cell boundary 
+int upBoundX; int lowBoundX;
+int upBoundY; int lowBoundY;
+int neighbors= 0;
+
 void setup() {
   Serial.begin(9600);
 
@@ -23,35 +28,84 @@ void setup() {
   
   display.clearDisplay();
   display.display();
-  memset(cells, 0, sizeof(cells[0][0]) * 128 * 64);
+//  memset(cells, 0, sizeof(cells[0][0]) * 128 * 64);
   cells[10][10] = 1;
+  cells[120][60] = 1;
   
 
 }
 
 void loop() {
   iterCells();  
-  delay(100);
+  delay(0);
 }
 
 void initCells() { // generate random cells
   
 }
 
-void iterCells() { // iterate cells
-//  display.clearDisplay();
+void iterCells() {
+  display.clearDisplay();
+  
+  for(i=0;i<128;i++){ // update cells
+    for(j=0;j<64;j++){    
+      if (cells[i][j]==1) {
+        display.drawPixel(i,j,WHITE);
+      }
+    }
+  }  
   
   for(i=0;i<128;i++){
-    for(j=0;j<64;j++){
-      Serial.println(cells[i][j]);
-    
-//      if (cells[i][j]) {
-//        Serial.println(i+","+j);
-//        display.drawPixel(i,j,WHITE);
-//      }
+    for(j=0;j<64;j++){    
+      if (cells[i][j]==1) {
+        display.drawPixel(i,j,WHITE);
+      }
     }
   }
-//  display.display();
+  display.setCursor(30,30);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.println("adf");
+  display.display();
   delay(200);
+  
+}
+
+int getCells(int x, int y, int cell) { // 1 = neighbor, 0 = empty
+  neighbors = 0;
+  if (x>0 and x<128) {
+    upBoundX= x-1;
+    lowBoundX= x+1;
+  }
+  else if (x==0){
+    upBoundX = x;
+    lowBoundX = x+1;
+  }
+  else if (x==128) {
+    upBoundX= x-1;
+    lowBoundX = x;
+  }
+  
+  if (y>0 and y<64) {
+    upBoundY= y-1;
+    lowBoundY= y+1;
+  }
+  else if (y==0){
+    upBoundY = y;
+    lowBoundY = y+1;
+  }
+  else if (y==128) {
+    upBoundY= y-1;
+    lowBoundY = y;
+  }
+  
+  for(i=upBoundX;i<lowBoundX;i++){
+    for(j=upBoundY;j<lowBoundY;j++){    
+      if (cells[i][j] == 1) neighbors++;
+    }
+  }
+    
+  if (cell) return neighbors;
+  else return (lowBoundX-upBoundX)*(lowBoundY-upBoundY)-neighbors;
   
 }
